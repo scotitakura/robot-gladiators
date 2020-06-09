efe188876a27305a428fa313400cc2187dcffa6b
sitakura <scot.itakura@gmail.com>
2020-06-09 02:07:28 -0700

    random fight order

--- assets/js/game.js
+++ assets/js/game.js
@@ -77,45 +77,47 @@ var fightOrSkip = function() {
 }
 
 var fight = function(enemy) {
+    var isPlayerTurn= true;
+    if (Math.random() > 0.5) {
+        isPlayerTurn = false;
+    }
+
     while(enemy.health > 0 && playerInfo.health > 0) {
-        if (fightOrSkip()) {
-           break;
-       }
-
-       var damage = randomNumber(playerInfo.attack -3, playerInfo.attack);
-
-        if (promptFight === "fight" || promptFight === "FIGHT") {
+        if (isPlayerTurn) {
+            if (fightOrSkip()) {
+                break;
+            }
 
             var damage = randomNumber(playerInfo.attack -3, playerInfo.attack);
             enemy.health = Math.max(0, enemy.health - damage);
             console.log(playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining.");
 
             if (enemy.health <= 0) {
-                window.alert(enemy.name + " has died!");
+                window.alert(enemy.name + " has died! You got 20 gold!");
                 playerInfo.money = playerInfo.money + 20;
                 break;
             } 
             else {
                 window.alert(enemy.name + " still has " + enemy.health + " health left.");
             }
-
+        }
+        else {
             var damage = randomNumber(enemy.attack - 3, enemy.attack);
             playerInfo.health = Math.max(0, playerInfo.health - damage);
             console.log(enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining.");
 
             if (playerInfo.health <= 0) {
                 window.alert(playerInfo.name + " has died!");
+                break;
             } 
             else {
                 window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
             } 
         }   
-
-        else {
-                window.alert("You need to pick a valid option. Try again!");
-        }
+        isPlayerTurn = !isPlayerTurn;
     }
 };
+
 var startGame = function () {
     playerInfo.reset();
     for (var i = 0; i < enemyInfo.length; i++) {
