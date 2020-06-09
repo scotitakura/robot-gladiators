c25d778e079a0429f2a98f6f4b21b5ee717ed7db
sitakura <scot.itakura@gmail.com>
2020-06-09 02:20:10 -0700

    highscores

--- assets/js/game.js
+++ assets/js/game.js
@@ -140,23 +140,33 @@ var startGame = function () {
     }
     endGame();
 }
+
 var endGame = function() {
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    window.alert("The game has now ended. Let's see how you did!");
+  
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+      highScore = 0;
+    }
+    if (playerInfo.money > highScore) {
+      localStorage.setItem("highscore", playerInfo.money);
+      localStorage.setItem("name", playerInfo.name);
+  
+      alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
     } 
     else {
-        window.alert("You've lost your robot in battle.");
+      alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
-
-    var playAgainConfirm = window.confirm("Would you like to play again?")
-
+  
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+  
     if (playAgainConfirm) {
-        startGame();
-    }
+      startGame();
+    } 
     else {
-        window.alert ("Thank you for playing Robot Gladiatos! Come back soon!")
+      window.alert("Thank you for playing Battlebots! Come back soon!");
     }
-};
+  };
 
 var shop = function() {
     var shopOptionPrompt = window.prompt (
